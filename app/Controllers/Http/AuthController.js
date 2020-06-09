f55e4b922fdf7f6850aef1c2ac7d70e9a960493d
Anderson Oliveira <contactandersonoliveira@gmail.com>
2020-06-09 19:16:54 -0300

    test:Auth

--- app/Controllers/Http/AuthController.js
+++ app/Controllers/Http/AuthController.js
@@ -5,28 +5,26 @@ const Unirest = require('unirest');
 const axios = require('axios');
 
 class AuthController {
-  async register( { request } ) {
-    // first async
+  async register({ request }) {
     const data = request.only([
-      'full_name', 
-      'email', 
-      'password', 
-      'telephone', 
-      'cep', 
-      'address', 
+      'full_name',
+      'email',
+      'password',
+      'telephone',
+      'cep',
+      'address',
       'latitude',
       'longitude',
-      'house_number', 
-      'reference', 
+      'house_number',
+      'reference',
       'type_account',
       'token_notification',
       'profile_picture'
-      ]);
-      let linkPicture = await this.getLink(data.profile_picture);  // another async 2°   
-      data.profile_picture = linkPicture;
-      // antes o 3° não espera o 2° acabar e o primeiro achava que tava tudo ok e retornava error
-      const user = await User.create(data); // another async
-      return user;
+    ]);
+    let linkPicture = await this.getLink(data.profile_picture);
+    data.profile_picture = linkPicture;
+    const user = await User.create(data);
+    return user;
   }
 
   async getLink(picture) {
@@ -34,75 +32,77 @@ class AuthController {
       url: "https://api.imgur.com/3/image",
       method: "post",
       headers: {
-        "Content-Type":"application/json",
-        "Authorization":"Client-ID 55ee05a412c4bae"
+        "Content-Type": "application/json",
+        "Authorization": "Client-ID 55ee05a412c4bae"
       },
       data: picture
-      }).then(function(response) {
-        return response.data.data.link;
-      }).catch(function(error) {
-        return error;
+    }).then(function (response) {
+      return response.data.data.link;
+    }).catch(function (error) {
+      return error;
     });
   }
-  
-  async index( { auth }){
-    const user = await User.query().where('id','=',auth.user.id).fetch()
+
+  async index({ auth }) {
+    const user = await User.query().where('id', '=', auth.user.id).fetch()
     return user
   }
-    
-    
-  async updateProfile( {request, auth} ) {
-  
-    let { 
-    email, 
-    telephone,
-    cep, 
-    address, 
-    latitude,
-    longitude,
-    house_number, 
-    reference,
-    profile_picture,
-    newProfileImage } = request.all(); 
 
-    if(newProfileImage == true){
-      let linkPicture = await this.getLink(profile_picture);      
-      profile_picture = linkPicture;  
+
+  async updateProfile({ request, auth }) {
+
+    const data = request.only([email,
+      'telephone',
+      'cep',
+      'address',
+      'latitude',
+      'longitude',
+      'house_number',
+      'reference',
+      'profile_picture']);
+
+      console.log(isNew);
+
+    if (data.profile_picture.match("/http/")) {
+      let linkPicture = await this.getLink(profile_picture);
+      profile_picture = linkPicture;
     }
-    
-    await Database
-      .where('id','=',auth.user.id).update({
-      email: email,
-      telephone: telephone,
-      cep: cep,
-      address: address,
-      latitude: latitude,
-      longitude: longitude,
-      house_number: house_number,
-      reference: reference,
-      profile_picture: profile_picture
-      })
-      .from('users')
+
+    return 'newImg';
+
+    // await Database
+    //   .where('id', '=', auth.user.id).update({
+    //     email: email,
+    //     telephone: telephone,
+    //     cep: cep,
+    //     address: address,
+    //     latitude: latitude,
+    //     longitude: longitude,
+    //     house_number: house_number,
+    //     reference: reference,
+    //     profile_picture: profile_picture
+    //   })
+    //   .from('users')
   }
-  
-  
-  async authenticate( { request, auth } ) { 
+
+
+  async authenticate({ request, auth }) {
     const { email, password, token_notification } = request.all();
-    
+
     const data = await Database.select('id')
       .select('type_account')
       .select('full_name')
       .select('status_account')
       .from('users').
-      whereIn("email", [email]);  
+      whereIn("email", [email]);
     const token_user = await auth.attempt(email, password);
-    
-    if(token_user.token.length > 0){
+
+    if (token_user.token.length > 0) {
       var updateToken = await Database
-      .table("users")
-      .where('email', '=', email)
-      .update('token_notification', token_notification)
-      .from("users");
+        .table("users")
+        .where('email', '=', email)
+        .update('token_notification', token_notification)
+        .from("users");
     }
 
     const user_id = data[0].id;
@@ -110,8 +110,8 @@ class AuthController {
     const type_account = data[0].type_account;
     const full_name = data[0].full_name
     const status_account = data[0].status_account
-    
-    return {token, user_id, type_account, full_name, status_account}
+
+    return { token, user_id, type_account, full_name, status_account }
   }
 }
 module.exports = AuthController
